ce25e471c27c58a778c5cf32ab65504271974a27
DeadByElpy <deadbyelpy@gmail.com>
2023-08-11 11:49:15 +0300

    update sendMessage implementation for TTN (#5)

--- src/ChirpStackServer.js
+++ src/ChirpStackServer.js
@@ -5,6 +5,7 @@ import getBase64FromBytes from 'jooby-codec/utils/getBase64FromBytes.js';
 
 
 const RESULT_LIMIT = 100;
+const NS_RELAY_NAMESPACE = 'chirpstack';
 const DEVICE_EUI_PROPERTY_NAME = 'devEui';
 
 // unique id for each device for sendMessage
@@ -91,7 +92,7 @@ const getDevice = async ( eui, {apiUrl, token} ) => {
 
 const sendData = async ( eui, data, {nsRelayUrl} ) => {
     const response = await fetch(
-        new URL(`/chirpstack/${eui}/messages`, nsRelayUrl),
+        new URL(`/${NS_RELAY_NAMESPACE}/${eui}/messages`, nsRelayUrl),
         {
             method: 'POST',
             headers: {
@@ -146,9 +147,10 @@ export default class ChirpStackServer extends Server {
         return device[DEVICE_EUI_PROPERTY_NAME];
     }
 
-    #mapDevice ( device, {application, tenant} ) {
+    #mapDevice ( device, options = {} ) {
         device.id = device[DEVICE_EUI_PROPERTY_NAME];
         const cachedDevice = this.devices[device.id];
+        const {application, tenant} = options;
 
         this.devices[device.id] = device;
         device.application = application || cachedDevice?.application;
@@ -211,7 +213,7 @@ export default class ChirpStackServer extends Server {
 
     addListener ( euiList, callback ) {
         const euiString = euiList.join();
-        const url = new URL(`/chirpstack/sse?eui=${euiString}`, this.config.nsRelayUrl);
+        const url = new URL(`/${NS_RELAY_NAMESPACE}/sse?eui=${euiString}`, this.config.nsRelayUrl);
         const eventSource = new EventSource(url.href);
 
         eventSource.onmessage = event => {
